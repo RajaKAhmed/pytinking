553ce31e50ce6df3ea6768c87b745059b7422448
Chief Thinkernologist <75857922+RajaKAhmed@users.noreply.github.com>
2023-07-17 17:30:59 -0400

    Delete test-openai-auth.py

--- llm/testing/test-openai-auth.py
+++
@@ -1,6 +0,0 @@
-import os
-import openai
-openai.organization = "org-AkUlB4dvZZooqXvZou0iX5ty"
-openai.api_key = ("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")
-x = openai.Model.list()
-print(x)
